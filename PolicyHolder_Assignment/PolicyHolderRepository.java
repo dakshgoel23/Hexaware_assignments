84e76bd7436608d73b407d36196418edd55f5875
dakshgoel23 <79467062+dakshgoel23@users.noreply.github.com>
2024-11-07 13:30:47 +0530

    Delete PolicyHolder_Assignment directory

--- PolicyHolder_Assignment/PolicyHolderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.springboot.insurance_app.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.springboot.insurance_app.model.PolicyHolder;
-
-public interface PolicyHolderRepository extends JpaRepository<PolicyHolder, Integer>{
-
-}
